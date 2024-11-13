def5faf4dafb6f86c5f45ac6968a43a3753d3e14
kochiling <112928080+kochiling@users.noreply.github.com>
2024-11-13 23:47:07 +0800

    Update login-backend.php

--- login-backend.php
+++ login-backend.php
@@ -29,11 +29,21 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
                 $row = mysqli_fetch_assoc($result);
 
                 if ($row) {
-                    if (password_verify($password, $row['password_hash'])) {
-                        $_SESSION['uid'] = $row['id'];
-                        $response['status'] = 'success';
-                        $response['role'] = $row['role'];
-                    } else {
+                    // Check if the password matches (CL added)
+                        if (password_verify($password, $row['password_hash'])) {
+                            // Check the data policy
+                            if ($row['data_policy'] == 0) {
+                                // User has not accepted the data policy
+                                $_SESSION['uid'] = $row['id'];
+                                $response['status'] = 'redirect';
+                                $response['page'] = 'update_policy.php';
+                            } else {
+                                // User has accepted the data policy, proceed with login
+                                $_SESSION['uid'] = $row['id'];
+                                $response['status'] = 'success';
+                                $response['role'] = $row['role'];
+                            }
+                        }else {
                         $response['status'] = 'error';
                         $response['message'] = 'Invalid email or password!';
                     }
