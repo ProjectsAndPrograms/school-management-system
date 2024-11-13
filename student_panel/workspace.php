7edb503ad13da246e854a3da939703685a5fd082
kochiling <112928080+kochiling@users.noreply.github.com>
2024-11-14 00:31:41 +0800

    Update workspace.php

--- student_panel/workspace.php
+++ student_panel/workspace.php
@@ -151,6 +151,10 @@ body::-webkit-scrollbar {
                 <span class="material-icons-sharp">description</span>
                 <h3>Workspace</h3>
             </a>
+            <a href="security_edu.php">
+                <span class="material-icons-sharp">security</span>
+                <h3>Security Education</h3>
+            </a>
             <a href="password.php">
                 <span class="material-icons-sharp">password</span>
                 <h3>Change Password</h3>
