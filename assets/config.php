da1df40a7582e448b1d2bf79517ab1dc15a76df4
mch3pg4 <mch3pg4@gmail.com>
2024-11-14 00:21:35 +0800

    added session expiration

--- assets/config.php
+++ assets/config.php
@@ -1,16 +1,13 @@
 <?php
-    $server = "localhost";
-   
-    $user = "root";
-    $password = "";
-    $db = "_sms";
-    
-    $conn = mysqli_connect($server, $user, $password, $db);
+$server = "localhost";
 
-    if (!$conn) {
-        header('Location: ../errors/error.html');
-        exit();
-    }
+$user = "root";
+$password = "root";
+$db = "_sms";
 
+$conn = mysqli_connect($server, $user, $password, $db);
 
-?>
+if (!$conn) {
+    header('Location: ../errors/error.html');
+    exit();
+}
